3a23d2ab15ed84e7c73b4e25dd2648dd1a1b0ba6
nabeff <neffina8@gmail.com>
2025-10-30 15:30:44 +0100

    font-tests

--- src/blocks/ComingSoonBlock/Component.tsx
+++ src/blocks/ComingSoonBlock/Component.tsx
@@ -44,8 +44,8 @@ export const ComingSoonBlock: React.FC<ComingSoonBlockType> = ({
       )}
 
       <div className="relative z-10 flex flex-col gap-12 lg:gap-0 mt-[40px] lg:mt-0 justify-center min-h-screen px-6 md:px-12 lg:px-24">
-        <div className="max-w-3xl flex flex-col items-start">
-          <h2 className="text-white text-4xl md:text-6xl lg:text-7xl mb-6">{title}</h2>
+        <div className="max-w-4xl flex flex-col items-start">
+          <h2 className="text-white text-4xl md:text-6xl lg:text-[84px] mb-6">{title}</h2>
 
           <div className="flex flex-col sm:flex-row gap-4">
             {primaryButton?.url && primaryButton?.label && (
