2959f583d7a71a78241b666965a171cb7b278cb7
AMGHAR-ELMAHDI <amgharmahdi1@gmail.com>
2025-11-25 20:41:51 +0100

    Added GTM tracking

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -7,12 +7,10 @@ import { Header } from '@/Header/Component'
 import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
-import { FacebookPixelScript } from '@/components/FaceBookScript'
 import { BreadcrumbLayout } from '@/components/Breadcrumb/BreadcrumbLayout'
 import 'react-loading-skeleton/dist/skeleton.css'
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
-import FloatingTicketCTA from '@/components/FloatingTicketCTA'
 import { UnityTicketsScript } from '@/components/UnityTicketsScript'
 import Script from 'next/script'
 
@@ -20,73 +18,25 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html className={cn(inter.variable)} lang="en" suppressHydrationWarning>
       <head>
-        {/* === Google Tag Manager: GTM-T93HFVSB (HEAD) === */}
+        {/* Google Tag Manager */}
         <Script
-          id="gtm-t93hfvb-head"
+          id="gtm-t93hfvsb"
           strategy="afterInteractive"
         >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T93HFVSB');`}</Script>
+        {/* End Google Tag Manager */}
 
-        {/* === End Google Tag Manager: GTM-T93HFVSB (HEAD) === */}
-
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9R48X46FZC"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-9R48X46FZC', {
-                'linker': {
-                  'domains': ['dupontregistry.com', 'leasing.dupontregistry.com']
-                }
-              });
-            `,
-          }}
-        />
-
-        {/* Google Tag Manager (existing TXPNNJ9C) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TXPNNJ9C');`,
-          }}
-        />
-        {/* Google Tag Manager (existing WLSNP8D) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WLSNP8D');`,
-          }}
-        />
-        {/* End existing Google Tag Managers */}
-
-        <FacebookPixelScript />
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
         <meta name="robots" content="index, follow" />
-        <meta
-          name="google-site-verification"
-          content="kpzRBe3JFD3txArPg0zrleykLgItyFiw-phvIjgH8Ag"
-        />
       </head>
 
       <body>
-        {/* === Google Tag Manager (noscript) : GTM-T93HFVSB === */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-T93HFVSB"
@@ -95,27 +45,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* === End Google Tag Manager (noscript) : GTM-T93HFVSB === */}
-
-        {/* Existing GTM noscripts – keep if you still need those containers */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TXPNNJ9C"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WLSNP8D"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End existing GTM noscripts */}
+        {/* End Google Tag Manager (noscript) */}
 
         <UnityTicketsScript />
         <Providers>
