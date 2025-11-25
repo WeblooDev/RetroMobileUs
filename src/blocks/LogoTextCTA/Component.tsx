d36bc8a5adff934e9db7196eac22f8a6d213d70e
AMGHAR-ELMAHDI <amgharmahdi1@gmail.com>
2025-11-25 20:55:53 +0100

    changed the default seo picture

--- src/blocks/LogoTextCTA/Component.tsx
+++ src/blocks/LogoTextCTA/Component.tsx
@@ -14,11 +14,11 @@ const LogoTextCTA: React.FC<LogoTextCTABlock> = ({
   return (
     <section className="container py-12">
       <div
-        className={`flex flex-col lg:flex-row gap-4 lg:gap-10 items-center justify-between ${reverse ? 'flex-row-reverse' : ''}`}
+        className={`flex flex-col lg:flex-row gap-6 lg:gap-10 items-center justify-between ${reverse ? 'flex-row-reverse' : ''}`}
       >
         <div className="w-full lg:w-[45%]">
-          <div className=" w-full ">
-            <Media resource={image}  imgClassName="object-cover" />
+          <div className="flex justify-start lg:justify-center w-full ">
+            <Media resource={image}  imgClassName="object-cover max-w-[358px]" />
           </div>
         </div>
 
